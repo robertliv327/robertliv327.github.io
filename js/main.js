921906bb05996db83db0a9bd2f9ee498339cf89a
Robert Livaudais <robert.h.livaudais.19@dartmouth.edu>
2018-02-22 14:21:58 -0500

    revamped

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-// taken from startbootstrap
+// js for my website
 
 $('document').ready(function($) {
   "use strict"; // Start of use strict
 
-  // Smooth scrolling using jQuery easing
+  // Smooth scrolling using jQuery easing - adopted from bootstrap.com
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
@@ -17,15 +17,29 @@ $('document').ready(function($) {
     }
   });
 
-  // Closes responsive menu when a scroll trigger link is clicked
+  // Closes responsive menu when a scroll trigger link is clicked - adopted from bootstrap.com
   $('.js-scroll-trigger').click(function() {
     $('.navbar-collapse').collapse('hide');
   });
 
-  // Activate scrollspy to add active class to navbar items on scroll
+  // Activate scrollspy to add active class to navbar items on scroll - adopted from bootstrap.com
   $('body').scrollspy({
     target: '#mainNav',
     offset: 54
   });
 
+  // Fade in navbar, make background opaque on scroll
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 40 ){
+      $('#mainNav').addClass('fadeIn');
+    } else {
+      $('#mainNav').removeClass('fadeIn');
+    };
+    $(".titleImage").css("opacity", 1 - $(window).scrollTop() / 900);  
+  });
+
+  // AOS setup
+  AOS.init({
+    duration: 1200,
+  })
 })(jQuery); // End of use strict
